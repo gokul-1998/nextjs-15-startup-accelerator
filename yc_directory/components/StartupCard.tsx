3eb4d95efde8d72c9d60cece36b1dc64428dc2e0
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-03 19:19:43 +0530

    2.12

--- yc_directory/components/StartupCard.tsx
+++ yc_directory/components/StartupCard.tsx
@@ -3,9 +3,10 @@ import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const StartupCard = ({post}:{post:StartupTypeCard}) => {
-    const {_createdAt,views,author:{_id:authorId,name},title,category,_id,image}=post
+    const {_createdAt,views,author:{_id:authorId,name},title,category,_id,image,description}=post
     return (
     <li className='startup-card group'>
         <div className="flex-between">
@@ -30,6 +31,25 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
             <Link href={`user/${authorId}`}>
                 <Image src='https://placehold.co/48x48' alt='placeholder' width={48} height={48} className='rounded-full'/>
             </Link>
+
+        </div>
+
+        <Link href={`/startup/${_id}`}>
+            <p className='startup-card_desc'>
+                {description}
+            </p>
+            <img src={image} alt={title} className='startup-card_img'/>
+        </Link>
+
+        <div className='flex-between gap-3 mt-5'>
+            <Link href={`/?query/${category.toLowerCase()}`}>
+                <p className='text-16-medium'>{category}</p>
+            </Link>
+            <Button className="startup-card_btn" asChild>
+                <Link href={`/startup/${_id}`}>
+                    Details
+                </Link>
+            </Button>
 
         </div>
 
