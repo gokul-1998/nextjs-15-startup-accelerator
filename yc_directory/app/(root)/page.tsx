fe337bda83363dfa5e484f96d07875facee4e8c1
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-03 17:51:33 +0530

    1.44

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -2,10 +2,18 @@
 export default function Home() {
   return (
     <>
+    <section className="pink_container">
+
     <h1 className="heading">
 
-    Home
+    Pitch Your Startup <br/> Connect with Entrepreneurs
     </h1>
+
+    <p className="sub-heading !max-w-3xl">
+      Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions.
+    </p>
+
+    </section>
     </>
   );
 }
