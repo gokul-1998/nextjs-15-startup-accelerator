39ef6891bfb6cdfd2d0ea35bbec9f0ef02433003
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-03 16:47:35 +0530

    error at 1.26.34

--- yc_directory/app/components/Navbar.tsx
+++ yc_directory/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { auth, signOut, signIn } from '@/auth';
+import { redirect } from 'next/dist/server/api-utils';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -18,7 +19,10 @@ const Navbar = async() => {
                             <Link href='/startup/create'>
                                 <span>Create</span>
                             </Link>
-                            <button onClick={signOut}>Sign Out</button>
+                            <form action={async()=>{
+                                'use server';
+                                await signOut(options:{redirectTo:'/'});
+                            }}>Sign Out</form>
                             <Link href={`/user/${session.id}`}>
                                 <span>{session.user.name}</span>
                             </Link>
